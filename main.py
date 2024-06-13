572ad7f925ff86d224ec2bc85cef0f3b203f5023
Arjun-G-Ravi <arjungravi1@gmail.com>
2024-06-13 12:38:12 +0530

    Completed the work

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ website = 'https://form.jotform.com/241635027272149'# 'https://form.jotform.com/
 driver.get(website)
 sleep(1)
 file_out, text_out= extract_name_and_xpath(website, driver)
-print(text_out)
+# print(text_out)
 questions = []
 for text_block in text_out:
     questions.append(text_block[1] + ' or ' + text_block[2])
 
-print(questions)
+# print(questions)
 
 llm_output = llm.generate(f'''You are an AI agent that is used to fill up application forms. Given som basic information, try to create a sample answer to the questions.
 Answer the question in the format starting with [ and ending with ]. Each element is separated by @@@ symbol, as shown below:
@@ -26,7 +26,7 @@ Dont output anything other than the answers. Remember to output in the correct f
 Make sure to answer every question in the following :{questions}.''')
 print(llm_output)
 
-print('-' *50)
+# print('-' *50)
 matches = re.findall(r'\[(.*?)\]', llm_output)[0]
 
 new = matches.split('@@@')
@@ -35,22 +35,25 @@ for i in new:
     answers.append(i)
 
 # print(answers)
-print(len(answers), len(questions))
+# print(len(answers), len(questions))
 for q,a in zip(questions, answers):
     print(q,' -> ',a)
 
-print('\n'*3)
+# print('\n'*3)
 for t,a in zip(text_out, answers):
-    print( t[0])
+    # print( t[0])
     el = driver.find_element(By.XPATH, t[0])
     el.send_keys(a)
     sleep(.1)
-
-
+# print('#'*30)
+# print(file_out[0][0])
 file_path ='/home/arjun/Desktop/GitHub/Autonomous-Form-Filler/resume to upload.pdf'
 # Locate the file input element and upload the file
-file_input = driver.find_element(By.XPATH, '//*[@id="input_17"]')
+file_input = driver.find_element(By.XPATH, file_out[0][0])
 file_input.send_keys(file_path)
 
-sleep(10)
+sleep(5)
+submit = driver.find_element(By.XPATH, '//*[@id="input_9"]')
+submit.click()
+sleep(2)
 driver.quit()
